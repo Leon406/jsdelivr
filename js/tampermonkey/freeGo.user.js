b28a3e57be20388fe97a37b91233b68c4f84e8de
Leon406 <deadogone@gmail.com>
2023-01-14 16:54:44 +0800

    commit via auto upload shell

--- js/tampermonkey/freeGo.user.js
+++ js/tampermonkey/freeGo.user.js
@@ -11,54 +11,100 @@
 
 const host = window.location.host
 
-const REAL_GO = {
-        "gitee.com": {
-             prefix: "https://gitee.com/link?target=",
-             query: "target",
-             action: urlDecode
-        }
-    }
-	
+    const REAL_GO = {
+    "gitee.com": {
+        prefix: "https://gitee.com/link?target=",
+        query: "target",
+        action: urlDecode
+    },
+     "sspai.com": {
+        prefix: "https://sspai.com/link?",
+        query: "target",
+        action: urlDecode
+    },
+    "www.youtube.com": {
+        prefix: "https://www.youtube.com/redirect?",
+        query: "q",
+        action: urlDecode
+    },
+    "mail.qq.com": {
+        fuc: () => get_elements("#contentDiv a").forEach(stopropagation)
+    },
+    "tieba.baidu.com": {
+        fuc: () => get_elements("#container a").forEach(stopropagation)
+    },
+}
+
 function findRule() {
-	
-	for (key in REAL_GO) {
-		
-	}
-}	
-	
-	
+
+    for (key in REAL_GO) {}
+}
+
+function find_all_iframe(doc = document) {
+    let frame = doc.querySelectorAll("iframe");
+    if (frame.length === 0)
+        return [];
+    let frames = Array.from(frame)
+        .filter(el => el.contentDocument)
+        if (frames.length > 0) {
+            let frames2 = frames.flatMap(el => find_all_iframe(el.contentDocument))
+                if (frames2.length > 0) {
+                    frames2.forEach(e => frames.push(e))
+                }
+        }
+        return frames;
+}
+
+function get_elements(selector, cond = el => el) {
+    let elements = Array.from(document.querySelectorAll(selector)).filter(cond);
+    if (elements.length === 0) {
+        elements = find_all_iframe()
+            .flatMap(el => Array.from(el.contentDocument.querySelectorAll(selector)))
+            .filter(el => cond(el));
+    }
+    return elements;
+}
+
+const stopEvent = (e) => {
+    console.log("eeee", e)
+    // 阻止事件冒泡, 因为上层元素绑定的click事件会重定向
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+};
+
 function stopropagation(aTag) {
-	if (aTag.onclick) {
-        aTag.onclick = (e) => {
-          // 阻止事件冒泡, 因为上层元素绑定的click事件会重定向
-          if (e.stopPropagation) {
-             e.stopPropagation();
-          }
-        };
-      }
-}	
- 
+    if (aTag.onclick && aTag.onclick != stopEvent)
+        aTag.onclick = stopEvent
+}
+
+function interval(fuc, timeout = 500) {
+    setInterval(fuc, timeout)
+}
 
 function urlDecode(aTag, query) {
-    aTag.href = decodeURIComponent( new URL(aTag.href).searchParams.get(query))
+    console.log("urlDecode", query, aTag)
+    aTag.href = decodeURIComponent(new URL(aTag.href).searchParams.get(query))
 }
 
- // 查询所有a标签
+// 查询所有a标签
 function findAllHref(rule = "http") {
-    return Array.from(document.querySelectorAll("a")).filter(el=> el.href.includes(rule))
+    return get_elements("a", el => el.href.includes(rule))
 }
 
-function findMore() {
-
-}
-
-(function() {
+(function () {
     'use strict';
-    let rule = REAL_GO[host];
-	 findAllHref("http").forEach(stopropagation);
-    if(rule) {
-         findAllHref(rule.prefix).forEach(el=>{
-           rule.action(el,rule.query)
-        });
+    window.onload = function () {
+        let rule = REAL_GO[host];
+        setTimeout(() => {
+            console.log("rule", rule)
+            if (rule) {
+                findAllHref(rule.prefix).forEach(el => {
+                    rule.action && rule.action(el, rule.query)
+                    rule.fuc && interval(rule.fuc)
+                });
+            }
+
+        }, 3000)
     }
 })();
--- js/tampermonkey/freeGo测试.md
+++ js/tampermonkey/freeGo测试.md
@@ -1,5 +1,17 @@
+# Free Go
+
+去除重定向或中间页面
 
 
-- https://gitee.com/ssssssss-team/spider-flow
-- 
+
+### 链接替换
+
+- [gitee](https://gitee.com/ssssssss-team/spider-flow) 
+- [少数派](https://sspai.com/post/77499)
+- YouTube
+
+### 轮询阻止事件冒泡
+
+- [贴吧](https://tieba.baidu.com/p/7763083667) 
+- [qq邮箱](https://mail.qq.com/)
 
