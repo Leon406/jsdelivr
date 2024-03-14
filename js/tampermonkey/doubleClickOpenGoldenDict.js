3b389f3c46e958d807564b0f9002090eb2ea5f42
Leon406 <deadogone@gmail.com>
2024-03-14 22:52:56 +0800

    commit via auto upload shell

--- js/tampermonkey/doubleClickOpenGoldenDict.js
+++ js/tampermonkey/doubleClickOpenGoldenDict.js
@@ -2,14 +2,14 @@
 // @name         Double Click Open GoldenDict
 // @namespace    http://tampermonkey.net/
 // @version      2024-03-14
-// @description  try to take over the world!
+// @description double click to query selected word in goldendict
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=goldendict.org
 // @author       Leon406
 // @license      AGPL-3.0-or-later
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
-
+ 
 (function () {
     'use strict';
     const getSelectionText = () => {
@@ -33,5 +33,5 @@
         }
         window.open("goldendict://" + getSelectionText())
     })
-
+ 
 })();
