6ec185c6bf476b07f05b83ad9fffba1b4c4d6226
Leon406 <deadogone@gmail.com>
2024-03-14 22:57:25 +0800

    commit via auto upload shell

--- js/tampermonkey/doubleClickOpenGoldenDict.js
+++ js/tampermonkey/doubleClickOpenGoldenDict.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Double Click Open GoldenDict
 // @namespace    http://tampermonkey.net/
-// @version      2024-03-14
+// @version      2024-03-15
 // @description double click to query selected word in goldendict
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=goldendict.org
 // @author       Leon406
@@ -12,26 +12,7 @@
  
 (function () {
     'use strict';
-    const getSelectionText = () => {
-        let text = getSelection().toString().trim();
-        if (!!text && /[a-z0-9]/i.test(text)) {
-            return text;
-        } else {
-            return null;
-        }
-    };
     document.addEventListener("dblclick", e => {
-        const ev = e.target;
-        if (ev.nodeName === "INPUT" && ev.getAttribute("type") === "password") {
-            const v = ev.value
-                ev.setAttribute("type", "text");
-            ev.value = v
-                setTimeout(() => {
-                    ev.setAttribute("type", "password")
-                }, 5000)
-                ev.onblur = () => ev.setAttribute("type", "password")
-        }
-        window.open("goldendict://" + getSelectionText())
+        window.open("goldendict://" + getSelection().toString().trim())
     })
- 
 })();
