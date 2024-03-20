4a39014f518d1143f0e52815d44e0780a96b94da
Leon Lambert <deadogone@gmail.com>
2024-03-20 11:48:31 +0800

    commit via auto upload shell

--- js/tampermonkey/doubleClickOpenGoldenDict.js
+++ js/tampermonkey/doubleClickOpenGoldenDict.js
@@ -1,18 +1,35 @@
 // ==UserScript==
 // @name         Double Click Open GoldenDict
 // @namespace    http://tampermonkey.net/
-// @version      2024-03-15
+// @version      2024-03-18
 // @description double click to query selected word in goldendict
+// @note  only support English 
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=goldendict.org
 // @author       Leon406
 // @license      AGPL-3.0-or-later
 // @match        *://*/*
-// @grant        none
+// @grant        GM_registerMenuCommand
+// @downloadURL https://update.greasyfork.org/scripts/489808/Double%20Click%20Open%20GoldenDict.user.js
+// @updateURL https://update.greasyfork.org/scripts/489808/Double%20Click%20Open%20GoldenDict.meta.js
 // ==/UserScript==
- 
+
 (function () {
     'use strict';
+	// 支持权限，改为 true
+	const selectAll = false
+    const getSelectionText = () => {
+        let text = getSelection().toString().trim();
+
+        if (!!text && ( selectAll|| /^[a-z']+$/i.test(text))) {
+            return text;
+        } else {
+            return null;
+        }
+    };
     document.addEventListener("dblclick", e => {
-        window.open("goldendict://" + getSelection().toString().trim())
+        let text = getSelectionText()
+            if (text) {
+                window.open("goldendict://" + text)
+            }
     })
 })();
